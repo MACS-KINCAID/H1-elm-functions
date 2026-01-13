0c0eac2a0175e46fefbce96a9e86a07f1f33a073
MACS-KINCAID <mitsiu.carreno@upa.edu.mx>
2026-01-13 12:37:40 -0600

    setup

--- README.md
+++ README.md
@@ -25,3 +25,69 @@ elm-test
 - elm-test 
 - elm-format
 - elm-review
+
+
+### Coding
+<!-- column_layout: [1,1] -->
+<!-- column: 0 -->
+Let's create a file "Helper.elm" in the "src" folder.
+```elm
+-- File: Ex1-functions/src/Helper
+module Helper exposing (..)
+
+double : number -> number
+double x =
+    x * 2
+
+```
+<!-- pause -->
+<!-- column: 1 -->
+Let's make sure our terminal is at "Ex1-functions" (pwd)
+```bash
+pwd
+elm repl
+```
+REPL stands for Read Eval Print Loop
+```bash
+import Helper
+
+Helper.double
+
+Helper.double 4
+
+:exit
+```
+
+<!-- reset_layout -->
+<!-- end_slide -->
+### More functions
+1. A new function "square" that takes a number and square's it (x^2)
+<!-- new_line -->
+2. A new function called "Greet" so that it receives a String name and return "Hello " + name
+<!-- new_line -->
+3. A new function "above5 : number -> Bool" that evaluates if a given number is greater than (>) 5
+<!-- end_slide -->
+<!-- jump_to_middle -->
+#### If
+#### More functions
+1. A new function "ifBoolTranslate" that get's a Bool and return a String either "Positivo" or "Negativo"
+<!-- new_line -->
+2. A new function "ifNumberSign" that get's a number and return a string either "Positive", "Negative", "Neutral"
+<!-- new_line -->
+
+<!-- end_slide -->
+
+<!-- jump_to_middle -->
+##### Case
+<!-- end_slide -->
+##### More functions
+<!-- column_layout: [1,1] -->
+<!-- column: 0 -->
+1. A new function "getNameById" that get's an Int and return a String
+
+![](./assets/getNameById.png)
+<!-- column: 1 -->
+2. A new function "getGradeByName" that get's a String and return a Float 
+
+![](./assets/getGradeByName.png)
+
---
+++ tests/TestSuite.elm
@@ -0,0 +1,156 @@
+module TestSuite exposing (..)
+
+import Expect exposing (Expectation)
+import Fuzz exposing (Fuzzer, int, list, niceFloat, string)
+import Helper exposing (..)
+import Test exposing (..)
+
+
+doubleFunc : Test
+doubleFunc =
+    describe "double function"
+        [ fuzz int "Double accept int" <|
+            \num ->
+                double num
+                    |> Expect.equal (num * 2)
+        , fuzz niceFloat "Double accept float" <|
+            \num ->
+                double num
+                    |> Expect.within (Expect.Absolute 0.000000001) (num * 2)
+        ]
+
+
+squareFunc : Test
+squareFunc =
+    describe "square function"
+        [ fuzz int "Square accept int" <|
+            \num ->
+                square num
+                    |> Expect.equal (num ^ 2)
+        , fuzz niceFloat "Square accept float" <|
+            \num ->
+                square num
+                    |> Expect.within (Expect.Absolute 0.000000001) (num ^ 2)
+        ]
+
+
+above5Func : Test
+above5Func =
+    describe "above5 function"
+        [ fuzz int "above5 accept int" <|
+            \num ->
+                above5 num
+                    |> Expect.equal (num > 5)
+        , fuzz niceFloat "above5 accept float" <|
+            \num ->
+                above5 num
+                    |> Expect.equal (num > 5)
+        ]
+
+
+boolTranslateFunc : Test
+boolTranslateFunc =
+    describe "boolTranslate function"
+        [ test "boolTranslate true" <|
+            \_ ->
+                boolTranslate True
+                    |> Expect.equal "Verdadero"
+        , test "boolTranslate false" <|
+            \_ ->
+                boolTranslate False
+                    |> Expect.equal "Falso"
+        ]
+
+
+numberSignFunc : Test
+numberSignFunc =
+    describe "numberSign function"
+        [ fuzz int "numberSign accept int" <|
+            \num ->
+                numberSign num
+                    |> Expect.equal
+                        (if num == 0 then
+                            "Neutral"
+
+                         else if num > 0 then
+                            "Positive"
+
+                         else
+                            "Negative"
+                        )
+        , fuzz niceFloat "numberSign accept float" <|
+            \num ->
+                numberSign num
+                    |> Expect.equal
+                        (if num == 0 then
+                            "Neutral"
+
+                         else if num > 0 then
+                            "Positive"
+
+                         else
+                            "Negative"
+                        )
+        ]
+
+
+intExcept123 : Fuzzer Int
+intExcept123 =
+    Fuzz.oneOf
+        [ Fuzz.intRange -100000 -1
+        , Fuzz.intRange 4 100000
+        ]
+
+
+getNameByIdFunc : Test
+getNameByIdFunc =
+    describe "getNameById function"
+        [ test "Id 1 -> Fernanda" <|
+            \_ ->
+                getNameById 1
+                    |> Expect.equal "Fernanda"
+        , test "Id 2 -> Miguel" <|
+            \_ ->
+                getNameById 2
+                    |> Expect.equal "Miguel"
+        , test "Id 3 -> Juan" <|
+            \_ ->
+                getNameById 3
+                    |> Expect.equal "Juan"
+        , fuzz intExcept123 "Id not 1, 2 or 3 -> Desconocido" <|
+            \num ->
+                getNameById num
+                    |> Expect.equal "Desconocido"
+        ]
+
+
+stringExcept : List String -> Fuzzer String
+stringExcept forbidden =
+    Fuzz.filter (\s -> not (List.member s forbidden)) Fuzz.string
+
+
+customStringFuzzer : Fuzzer String
+customStringFuzzer =
+    stringExcept [ "Fernanda", "Miguel", "Juan" ]
+
+
+getGradeByNameFunc : Test
+getGradeByNameFunc =
+    describe "getGradeByName function"
+        [ test "Fernanda -> 9.4" <|
+            \_ ->
+                getGradeByName "Fernanda"
+                    |> Expect.within (Expect.Absolute 0.000000001) 9.4
+        , test "Miguel -> 8.7" <|
+            \_ ->
+                getGradeByName "Miguel"
+                    |> Expect.within (Expect.Absolute 0.000000001) 8.7
+        , test "Juan -> 9.3" <|
+            \_ ->
+                getGradeByName "Juan"
+                    |> Expect.within (Expect.Absolute 0.000000001) 9.3
+        , fuzz customStringFuzzer "Random string except Fernanda, Miguel or Juan -> 0" <|
+            \name ->
+                getGradeByName name
+                    |> Expect.within (Expect.Absolute 0.000000001) 0
+        ]
